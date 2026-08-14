edd839fb585143dcc28d27fb9415a0babb8fffe0
TechHassan06 <muhammadhassan67895@gmail.com>
2026-08-15 02:42:12 +0500

    Complete GoCart AI Multi-Vendor project setup with Groq AI

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,15 @@
 'use client'
-import { Search, ShoppingCart } from "lucide-react";
+import { PackageIcon, Search, ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import {useUser, useClerk, UserButton, Protect} from "@clerk/nextjs"
 
 const Navbar = () => {
 
+    const {user} = useUser()
+    const {openSignIn} = useClerk()
     const router = useRouter();
 
     const [search, setSearch] = useState('')
@@ -24,9 +27,11 @@ const Navbar = () => {
 
                     <Link href="/" className="relative text-4xl font-semibold text-slate-700">
                         <span className="text-green-600">go</span>cart<span className="text-green-600 text-5xl leading-0">.</span>
-                        <p className="absolute text-xs font-semibold -top-1 -right-8 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-green-500">
+                       <Protect plan='plus'>
+                         <p className="absolute text-xs font-semibold -top-1 -right-8 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-green-500">
                             plus
                         </p>
+                       </Protect>
                     </Link>
 
                     {/* Desktop Menu */}
@@ -47,17 +52,44 @@ const Navbar = () => {
                             <button className="absolute -top-1 left-3 text-[8px] text-white bg-slate-600 size-3.5 rounded-full">{cartCount}</button>
                         </Link>
 
-                        <button className="px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
+                       {!user ? (
+                         <button onClick={openSignIn} className="px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
                             Login
                         </button>
+                       ) : (
+                        <UserButton>
+                            <UserButton.MenuItems>
+                                <UserButton.Action labelIcon={<PackageIcon size={16}/>} label="My Orders" onClick={()=> router.push('/orders')}/>
+                            </UserButton.MenuItems>
+                        </UserButton>
+                       )}
 
                     </div>
 
                     {/* Mobile User Button  */}
                     <div className="sm:hidden">
-                        <button className="px-7 py-1.5 bg-indigo-500 hover:bg-indigo-600 text-sm transition text-white rounded-full">
+
+                        {user ? (
+                            <div>
+                                   <UserButton>
+                            <UserButton.MenuItems>
+                                <UserButton.Action labelIcon={<ShoppingCart size={16}/>} label="Cart" onClick={()=> router.push('/cart')}/>
+                            </UserButton.MenuItems>
+                        </UserButton>
+
+                           <UserButton>
+                            <UserButton.MenuItems>
+                                <UserButton.Action labelIcon={<PackageIcon size={16}/>} label="My Orders" onClick={()=> router.push('/orders')}/>
+                            </UserButton.MenuItems>
+                        </UserButton>
+
+                            </div>
+                        ) : (
+                               <button onClick={openSignIn} className="px-7 py-1.5 bg-indigo-500 hover:bg-indigo-600 text-sm transition text-white rounded-full">
                             Login
                         </button>
+                        )}
+                      
                     </div>
                 </div>
             </div>
